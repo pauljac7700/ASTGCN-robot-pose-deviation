d0fd9c93cfcac105ffaaa86601e6dcf5aa6be8f7
pauljac7700 <118752887+pauljac7700@users.noreply.github.com>
2024-11-10 18:00:32 +0800

    Head_start

---
+++ build_graph.py
@@ -0,0 +1,49 @@
+import numpy as np
+import yaml
+
+def build_adjacency_matrix(config):
+    """
+    Builds the adjacency matrix for the graph representing the robotic system.
+    
+    Nodes:
+    - Nodes 0-5: Joint nodes
+    - Node 6: End-effector input node
+    - Node 7: Error node (x_dif)
+
+    Connections:
+    - Each joint node is connected to the next joint node like a chain.
+    - Each joint node is connected to the end-effector input node.
+    - All nodes are connected to the error node.
+    - All connections have a weight/value of 1.
+    """
+    num_nodes = config['model']['num_of_vertices'] # 6 joint nodes + 1 end-effector input node + 1 error node
+    adjacency_matrix = np.zeros((num_nodes, num_nodes))
+
+    # Connect joint nodes in a chain
+    for i in range(5):
+        adjacency_matrix[i, i+1] = 1
+        adjacency_matrix[i+1, i] = 1
+
+    # Connect each joint node (0-5) to the end-effector input node (6)
+    for joint_node in range(6):
+        adjacency_matrix[joint_node, 6] = 1
+        adjacency_matrix[6, joint_node] = 1
+
+    # Connect all nodes to the error node (7)
+    for node in range(num_nodes - 1):  # Nodes 0-6
+        adjacency_matrix[node, 7] = 1
+        adjacency_matrix[7, node] = 1
+
+    # Since the connections are bidirectional and have a weight of 1, the adjacency matrix is symmetric
+    return adjacency_matrix
+
+if __name__ == "__main__":
+    # Load configuration
+    with open('config.yaml') as f:
+        config = yaml.safe_load(f)
+    
+    adj_matrix = build_adjacency_matrix(config)
+    np.save('data/adjacency_matrix.npy', adj_matrix)
+    print("Adjacency matrix saved to 'data/adjacency_matrix.npy'")
+    print("Adjacency Matrix:")
+    print(adj_matrix)
---
+++ model/ConvLSTM.py
@@ -0,0 +1,476 @@
+# -*- coding:utf-8 -*-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from lib.utils import scaled_Laplacian, cheb_polynomial
+
+
+class Spatial_Attention_layer(nn.Module):
+    '''
+    compute spatial attention scores
+    '''
+    def __init__(self, DEVICE, in_channels, num_of_vertices, num_of_timesteps):
+        super(Spatial_Attention_layer, self).__init__()
+        self.W1 = nn.Parameter(torch.FloatTensor(num_of_timesteps).to(DEVICE))
+        self.W2 = nn.Parameter(torch.FloatTensor(in_channels, num_of_timesteps).to(DEVICE))
+        self.W3 = nn.Parameter(torch.FloatTensor(in_channels).to(DEVICE))
+        self.bs = nn.Parameter(torch.FloatTensor(1, num_of_vertices, num_of_vertices).to(DEVICE))
+        self.Vs = nn.Parameter(torch.FloatTensor(num_of_vertices, num_of_vertices).to(DEVICE))
+
+
+    def forward(self, x):
+        '''
+        :param x: (batch_size, N, F_in, T)
+        :return: (B,N,N)
+        '''
+
+        lhs = torch.matmul(torch.matmul(x, self.W1), self.W2)  # (b,N,F,T)(T)->(b,N,F)(F,T)->(b,N,T)
+
+        rhs = torch.matmul(self.W3, x).transpose(-1, -2)  # (F)(b,N,F,T)->(b,N,T)->(b,T,N)
+
+        product = torch.matmul(lhs, rhs)  # (b,N,T)(b,T,N) -> (B, N, N)
+
+        S = torch.matmul(self.Vs, torch.sigmoid(product + self.bs))  # (N,N)(B, N, N)->(B,N,N)
+
+        S_normalized = F.softmax(S, dim=1)
+
+        return S_normalized
+
+
+class cheb_conv_withSAt(nn.Module):
+    '''
+    K-order chebyshev graph convolution
+    '''
+
+    def __init__(self, K, cheb_polynomials, in_channels, out_channels):
+        '''
+        :param K: int
+        :param in_channles: int, num of channels in the input sequence
+        :param out_channels: int, num of channels in the output sequence
+        '''
+        super(cheb_conv_withSAt, self).__init__()
+        self.K = K
+        self.cheb_polynomials = cheb_polynomials
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.DEVICE = cheb_polynomials[0].device
+        self.Theta = nn.ParameterList([nn.Parameter(torch.FloatTensor(in_channels, out_channels).to(self.DEVICE)) for _ in range(K)])
+
+    def forward(self, x, spatial_attention):
+        '''
+        Chebyshev graph convolution operation
+        :param x: (batch_size, N, F_in, T)
+        :return: (batch_size, N, F_out, T)
+        '''
+
+        batch_size, num_of_vertices, in_channels, num_of_timesteps = x.shape
+
+        outputs = []
+
+        for time_step in range(num_of_timesteps):
+
+            graph_signal = x[:, :, :, time_step]  # (b, N, F_in)
+
+            output = torch.zeros(batch_size, num_of_vertices, self.out_channels).to(self.DEVICE)  # (b, N, F_out)
+
+            for k in range(self.K):
+
+                T_k = self.cheb_polynomials[k]  # (N,N)
+
+                T_k_with_at = T_k.mul(spatial_attention)   # (N,N)*(N,N) = (N,N) 多行和为1, 按着列进行归一化
+
+                theta_k = self.Theta[k]  # (in_channel, out_channel)
+
+                rhs = T_k_with_at.permute(0, 2, 1).matmul(graph_signal)  # (N, N)(b, N, F_in) = (b, N, F_in) 因为是左乘，所以多行和为1变为多列和为1，即一行之和为1，进行左乘
+
+                output = output + rhs.matmul(theta_k)  # (b, N, F_in)(F_in, F_out) = (b, N, F_out)
+
+            outputs.append(output.unsqueeze(-1))  # (b, N, F_out, 1)
+
+        return F.relu(torch.cat(outputs, dim=-1))  # (b, N, F_out, T)
+
+
+class Temporal_Attention_layer(nn.Module):
+    def __init__(self, DEVICE, in_channels, num_of_vertices, num_of_timesteps):
+        super(Temporal_Attention_layer, self).__init__()
+        self.U1 = nn.Parameter(torch.FloatTensor(num_of_vertices).to(DEVICE))
+        self.U2 = nn.Parameter(torch.FloatTensor(in_channels, num_of_vertices).to(DEVICE))
+        self.U3 = nn.Parameter(torch.FloatTensor(in_channels).to(DEVICE))
+        self.be = nn.Parameter(torch.FloatTensor(1, num_of_timesteps, num_of_timesteps).to(DEVICE))
+        self.Ve = nn.Parameter(torch.FloatTensor(num_of_timesteps, num_of_timesteps).to(DEVICE))
+
+    def forward(self, x):
+        '''
+        :param x: (batch_size, N, F_in, T)
+        :return: (B, T, T)
+        '''
+        _, num_of_vertices, num_of_features, num_of_timesteps = x.shape
+
+        lhs = torch.matmul(torch.matmul(x.permute(0, 3, 2, 1), self.U1), self.U2)
+        # x:(B, N, F_in, T) -> (B, T, F_in, N)
+        # (B, T, F_in, N)(N) -> (B,T,F_in)
+        # (B,T,F_in)(F_in,N)->(B,T,N)
+
+        rhs = torch.matmul(self.U3, x)  # (F)(B,N,F,T)->(B, N, T)
+
+        product = torch.matmul(lhs, rhs)  # (B,T,N)(B,N,T)->(B,T,T)
+
+        E = torch.matmul(self.Ve, torch.sigmoid(product + self.be))  # (B, T, T)
+
+        E_normalized = F.softmax(E, dim=1)
+
+        return E_normalized
+
+
+class cheb_conv(nn.Module):
+    '''
+    K-order chebyshev graph convolution
+    '''
+
+    def __init__(self, K, cheb_polynomials, in_channels, out_channels):
+        '''
+        :param K: int
+        :param in_channles: int, num of channels in the input sequence
+        :param out_channels: int, num of channels in the output sequence
+        '''
+        super(cheb_conv, self).__init__()
+        self.K = K
+        self.cheb_polynomials = cheb_polynomials
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.DEVICE = cheb_polynomials[0].device
+        self.Theta = nn.ParameterList([nn.Parameter(torch.FloatTensor(in_channels, out_channels).to(self.DEVICE)) for _ in range(K)])
+
+    def forward(self, x):
+        '''
+        Chebyshev graph convolution operation
+        :param x: (batch_size, N, F_in, T)
+        :return: (batch_size, N, F_out, T)
+        '''
+
+        batch_size, num_of_vertices, in_channels, num_of_timesteps = x.shape
+
+        outputs = []
+
+        for time_step in range(num_of_timesteps):
+
+            graph_signal = x[:, :, :, time_step]  # (b, N, F_in)
+
+            output = torch.zeros(batch_size, num_of_vertices, self.out_channels).to(self.DEVICE)  # (b, N, F_out)
+
+            for k in range(self.K):
+
+                T_k = self.cheb_polynomials[k]  # (N,N)
+
+                theta_k = self.Theta[k]  # (in_channel, out_channel)
+
+                rhs = graph_signal.permute(0, 2, 1).matmul(T_k).permute(0, 2, 1)
+
+                output = output + rhs.matmul(theta_k)
+
+            outputs.append(output.unsqueeze(-1))
+
+        return F.relu(torch.cat(outputs, dim=-1))
+
+
+class ASTGCN_block(nn.Module):
+
+    def __init__(self, DEVICE, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, cheb_polynomials, num_of_vertices, num_of_timesteps):
+        super(ASTGCN_block, self).__init__()
+        self.TAt = Temporal_Attention_layer(DEVICE, in_channels, num_of_vertices, num_of_timesteps)
+        self.SAt = Spatial_Attention_layer(DEVICE, in_channels, num_of_vertices, num_of_timesteps)
+        self.cheb_conv_SAt = cheb_conv_withSAt(K, cheb_polynomials, in_channels, nb_chev_filter)
+        self.time_conv = nn.Conv2d(nb_chev_filter, nb_time_filter, kernel_size=(1, 3), stride=(1, time_strides), padding=(0, 1))
+        self.residual_conv = nn.Conv2d(in_channels, nb_time_filter, kernel_size=(1, 1), stride=(1, time_strides))
+        self.ln = nn.LayerNorm(nb_time_filter)  #需要将channel放到最后一个维度上
+
+    def forward(self, x):
+        '''
+        :param x: (batch_size, N, F_in, T)
+        :return: (batch_size, N, nb_time_filter, T)
+        '''
+        batch_size, num_of_vertices, num_of_features, num_of_timesteps = x.shape
+
+        # TAt
+        temporal_At = self.TAt(x)  # (b, T, T)
+
+        x_TAt = torch.matmul(x.reshape(batch_size, -1, num_of_timesteps), temporal_At).reshape(batch_size, num_of_vertices, num_of_features, num_of_timesteps)
+
+        # SAt
+        spatial_At = self.SAt(x_TAt)
+
+        # cheb gcn
+        spatial_gcn = self.cheb_conv_SAt(x, spatial_At)  # (b,N,F,T)
+        # spatial_gcn = self.cheb_conv(x)
+
+        # convolution along the time axis
+        time_conv_output = self.time_conv(spatial_gcn.permute(0, 2, 1, 3))  # (b,N,F,T)->(b,F,N,T) 用(1,3)的卷积核去做->(b,F,N,T)
+
+        # residual shortcut
+        x_residual = self.residual_conv(x.permute(0, 2, 1, 3))  # (b,N,F,T)->(b,F,N,T) 用(1,1)的卷积核去做->(b,F,N,T)
+
+        x_residual = self.ln(F.relu(x_residual + time_conv_output).permute(0, 3, 2, 1)).permute(0, 2, 3, 1)
+        # (b,F,N,T)->(b,T,N,F) -ln-> (b,T,N,F)->(b,N,F,T)
+
+        return x_residual
+
+
+class ASTGCN_submodule(nn.Module):
+
+    def __init__(self, DEVICE, nb_block, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, cheb_polynomials, num_for_predict, len_input, num_of_vertices):
+        '''
+        :param nb_block:
+        :param in_channels:
+        :param K:
+        :param nb_chev_filter:
+        :param nb_time_filter:
+        :param time_strides:
+        :param cheb_polynomials:
+        :param nb_predict_step:
+        '''
+
+        super(ASTGCN_submodule, self).__init__()
+
+        self.BlockList = nn.ModuleList([ASTGCN_block(DEVICE, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, cheb_polynomials, num_of_vertices, len_input)])
+
+        self.BlockList.extend([ASTGCN_block(DEVICE, nb_time_filter, K, nb_chev_filter, nb_time_filter, 1, cheb_polynomials, num_of_vertices, len_input//time_strides) for _ in range(nb_block-1)])
+
+        self.final_conv = nn.Conv2d(int(len_input/time_strides), num_for_predict, kernel_size=(1, nb_time_filter))
+
+        self.DEVICE = DEVICE
+
+        self.to(DEVICE)
+
+    def forward(self, x):
+        '''
+        :param x: (B, N_nodes, F_in, T_in)
+        :return: (B, N_nodes, T_out)
+        '''
+        for block in self.BlockList:
+            x = block(x)
+
+        output = self.final_conv(x.permute(0, 3, 1, 2))[:, :, :, -1].permute(0, 2, 1)
+        # (b,N,F,T)->(b,T,N,F)-conv<1,F>->(b,c_out*T,N,1)->(b,c_out*T,N)->(b,N,T)
+
+        return output
+
+
+def make_model(DEVICE, nb_block, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, adj_mx, num_for_predict, len_input, num_of_vertices):
+    '''
+
+    :param DEVICE:
+    :param nb_block:
+    :param in_channels:
+    :param K:
+    :param nb_chev_filter:
+    :param nb_time_filter:
+    :param time_strides:
+    :param cheb_polynomials:
+    :param nb_predict_step:
+    :param len_input
+    :return:
+    '''
+    L_tilde = scaled_Laplacian(adj_mx)
+    cheb_polynomials = [torch.from_numpy(i).type(torch.FloatTensor).to(DEVICE) for i in cheb_polynomial(L_tilde, K)]
+    model = ASTGCN_submodule(DEVICE, nb_block, in_channels, K, nb_chev_filter, nb_time_filter, time_strides, cheb_polynomials, num_for_predict, len_input, num_of_vertices)
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+        else:
+            nn.init.uniform_(p)
+
+    return model
+
+
+import torch.nn as nn
+import torch
+
+
+class ConvLSTMCell(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, bias):
+        """
+        Initialize ConvLSTM cell.
+
+        Parameters
+        ----------
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+
+        super(ConvLSTMCell, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def forward(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, image_size):
+        height, width = image_size
+        return (torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, height, width, device=self.conv.weight.device))
+
+
+class ConvLSTM(nn.Module):
+
+    """
+
+    Parameters:
+        input_dim: Number of channels in input
+        hidden_dim: Number of hidden channels
+        kernel_size: Size of kernel in convolutions
+        num_layers: Number of LSTM layers stacked on each other
+        batch_first: Whether or not dimension 0 is the batch or not
+        bias: Bias or no bias in Convolution
+        return_all_layers: Return the list of computations for all layers
+        Note: Will do same padding.
+
+    Input:
+        A tensor of size B, T, C, H, W or T, B, C, H, W
+    Output:
+        A tuple of two lists of length num_layers (or length 1 if return_all_layers is False).
+            0 - layer_output_list is the list of lists of length T of each output
+            1 - last_state_list is the list of last states
+                    each element of the list is a tuple (h, c) for hidden state and memory
+    Example:
+        >> x = torch.rand((32, 10, 64, 128, 128))
+        >> convlstm = ConvLSTM(64, 16, 3, 1, True, True, False)
+        >> _, last_states = convlstm(x)
+        >> h = last_states[0][0]  # 0 for layer index, 0 for h index
+    """
+
+    def __init__(self, input_dim, hidden_dim, kernel_size, num_layers,
+                 batch_first=False, bias=True, return_all_layers=False):
+        super(ConvLSTM, self).__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+
+        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == num_layers
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.bias = bias
+        self.return_all_layers = return_all_layers
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim,
+                                          hidden_dim=self.hidden_dim[i],
+                                          kernel_size=self.kernel_size[i],
+                                          bias=self.bias))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, input_tensor, hidden_state=None):
+        """
+
+        Parameters
+        ----------
+        input_tensor: todo
+            5-D Tensor either of shape (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state: todo
+            None. todo implement stateful
+
+        Returns
+        -------
+        last_state_list, layer_output
+        """
+        if not self.batch_first:
+            # (t, b, c, h, w) -> (b, t, c, h, w)
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+
+        b, _, _, h, w = input_tensor.size()
+
+        # Implement stateful ConvLSTM
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            # Since the init is done in forward. Can send image size here
+            hidden_state = self._init_hidden(batch_size=b,
+                                             image_size=(h, w))
+
+        layer_output_list = []
+        last_state_list = []
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                 cur_state=[h, c])
+                output_inner.append(h)
+
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])
+
+        if not self.return_all_layers:
+            layer_output_list = layer_output_list[-1:]
+            last_state_list = last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+    def _init_hidden(self, batch_size, image_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, image_size))
+        return init_states
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or
+                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('`kernel_size` must be tuple or list of tuples')
+
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
